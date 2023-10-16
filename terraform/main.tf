9673cefce745d5f883bf0123766a2f2e66e68ad2
Thang <tranducthang0927@gmail.com>
2023-10-16 15:38:58 +1100

    testing pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,10 +4,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 3.27"
     }
   }
-
   required_version = ">= 1.2.0"
 }
 
@@ -15,34 +14,68 @@ provider "aws" {
   region  = "ap-southeast-2"
 }
 
+
+
 resource "aws_instance" "app" {
-  ami           = "ami-0e812285fd54f7620"
-  instance_type = "t2.micro"
-  key_name      = "ass2keypair"
+  ami           = "ami-00e734696834c1dcc"
+  instance_type = "t4g.small"
+  key_name = "assignment"
+  security_groups = ["test"]
 
   tags = {
-    Name = "ExampleAppServerInstance"
-  }
-
-
-  provisioner "remote-exec" {
-    
-    inline = [
-      "sudo yum update -y",
-      "sudo amazon-linux-extras install docker",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-      "sudo usermod -aG docker ec2-user",
-      "sudo systemctl start docker",
-      "sudo yum install -y docker-compose",
-    ]
-    
-    connection {
-    type = "ssh"
-    user = "ec2-user"
-    private_key = file("ass2keypair.pem")
-    host = self.public_ip
-    }
+    Name = "testServer"
   }
 }
 
+
+# Provision the instance
+resource "null_resource" "docker" {
+  triggers = {
+    instance_id = aws_instance.app.id
+  }
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.app.public_ip
+    user        = "ec2-user"
+    private_key = file("./key/assignment2.pem")
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo yum install docker -y",
+      "sudo service docker start",
+      "sudo chkconfig docker on",
+      "sudo yum install -y git",
+      "sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
+      "sudo chmod +x /usr/local/bin/docker-compose",
+      "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose"
+    ]
+  }
+}
+
+# Define Docker Compose configuration
+resource "null_resource" "docker_run" {
+  depends_on = [null_resource.docker]
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.app.public_ip
+    user        = "ec2-user"
+    private_key = file("./key/assignment2.pem")
+
+  }
+
+  provisioner "file" {
+    source      = "../docker/docker-compose.yml"  # Replace with your Docker Compose file
+    destination = "/home/ec2-user/docker-compose.yml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cd /home/ec2-user",
+      "sudo docker-compose up -d",
+    ]
+  }
+}
