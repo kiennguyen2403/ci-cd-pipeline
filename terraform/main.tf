cd6952e0fc8d235f7b6c972e361f8c7e5cb96720
kiennguyen2403 <kientqk2403@example.com>
2023-10-16 12:16:50 +1100

    changing main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,5 @@
+# main.tf
+
 terraform {
   required_providers {
     aws = {
@@ -13,11 +15,72 @@ provider "aws" {
   region  = "ap-southeast-2"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "app" {
   ami           = "ami-0e812285fd54f7620"
   instance_type = "t2.micro"
 
   tags = {
     Name = "ExampleAppServerInstance"
+  }
+}
+
+# Open SSH port
+resource "aws_security_group" "example" {
+  name        = "example"
+  description = "Example security group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Provision the instance
+resource "null_resource" "docker" {
+  triggers = {
+    instance_id = aws_instance.app.id
+  }
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.app.public_ip
+    user        = "ec2-user"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install docker",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker",
+      "sudo usermod -aG docker ec2-user",
+      "sudo systemctl start docker",
+      "sudo yum install -y docker-compose",
+    ]
+  }
+}
+
+# Define Docker Compose configuration
+resource "null_resource" "docker_run" {
+  depends_on = [null_resource.docker]
+
+  connection {
+    type        = "ssh"
+    host        = aws_instance.app.public_ip
+    user        = "ec2-user"
+  }
+
+  provisioner "file" {
+    source      = "../docker/docker-compose.yml"  # Replace with your Docker Compose file
+    destination = "/home/ec2-user/docker-compose.yml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "cd /home/ec2-user",
+      "docker-compose up -d",
+    ]
   }
 }
