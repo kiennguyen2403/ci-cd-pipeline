404f1c53be4c5b8a53ae5d24865e2e1b476a9bcc
kiennguyen2403 <kientqk2403@example.com>
2023-10-16 12:05:41 +1100

    changing main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,8 +5,12 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with your desired AMI ID
+  ami           = "ami-0e812285fd54f7620"
   instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
 }
 
 # Open SSH port
